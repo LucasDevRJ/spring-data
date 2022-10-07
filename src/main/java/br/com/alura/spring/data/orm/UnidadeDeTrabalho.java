099f8955747ee36d5f7c78bafeac3acb63b8fe0f
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-07 11:37:09 -0300

    Criação da classe Unidade de Trabalho

---
+++ src/main/java/br/com/alura/spring/data/orm/UnidadeDeTrabalho.java
@@ -0,0 +1,28 @@
+package br.com.alura.spring.data.orm;
+
+public class UnidadeDeTrabalho {
+	
+	private Integer id;
+	private String descricao;
+	private String endereco;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+}
