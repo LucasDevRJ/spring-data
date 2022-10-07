ebed71c14a12668ec5c22689fbfb7e8d7e671052
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-07 09:46:00 -0300

    Criação do método para atualizar dados

--- src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
+++ src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
@@ -28,4 +28,17 @@ public class CrudCargoService {
 		cargoRepository.save(cargo);
 		System.out.println("Cargo salvo com sucesso!");
 	}
+	
+	private void atualizar(Scanner scanner) {
+		System.out.println("ID");
+		int id = scanner.nextInt();
+		System.out.println("Descrição do cargo");
+		String descricao = scanner.next();
+		
+		Cargo cargo = new Cargo();
+		cargo.setId(id);
+		cargo.setDescricao(descricao);
+		cargoRepository.save(cargo);
+		System.out.println("Cargo atualizado com sucesso!");
+	}
 }
