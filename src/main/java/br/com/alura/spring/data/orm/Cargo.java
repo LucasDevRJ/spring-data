b7ade075d1b0446276b3583265dda1feb37fe02d
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-06 21:53:49 -0300

    Criação da entidade por meio da classe

--- src/main/java/br/com/alura/spring/data/orm/Cargo.java
+++ src/main/java/br/com/alura/spring/data/orm/Cargo.java
@@ -1,5 +1,10 @@
 package br.com.alura.spring.data.orm;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity //Indica ao Spring Data que esta classe é uma entidade, para assim criar uma entidade a partir desta classe
+@Table(name = "cargos") //Permite alterar atributos da entidade criadas, como nesse caso, o nome
 public class Cargo {
 
 }
