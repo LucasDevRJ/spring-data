5243d0384f5457af1f66d38d064de506f4813544
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-06 21:03:17 -0300

    Adição de comentários no código e adição da dependência do Banco

--- src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -3,10 +3,11 @@ package br.com.alura.spring.data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //serve para quando iniciado o Spring percorra as anotações que temos para executa-las
 public class SpringDataApplication {
 
 	public static void main(String[] args) {
+		//serve para iniciar o Spring
 		SpringApplication.run(SpringDataApplication.class, args);
 	}
 
