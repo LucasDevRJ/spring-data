b1e7a98374531bfd5d62fa7c883aadd91257d144
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-07 09:31:21 -0300

    Função de registar dados no banco por meio do Java concluída

--- src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -1,5 +1,6 @@
 package br.com.alura.spring.data;
 
+import java.security.DrbgParameters.NextBytes;
 import java.util.Scanner;
 
 import org.springframework.boot.CommandLineRunner;
@@ -7,17 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.alura.spring.data.orm.Cargo;
-import br.com.alura.spring.data.repository.CargoRepository;
+import br.com.alura.spring.data.service.CrudCargoService;
 
 @SpringBootApplication //serve para quando iniciado o Spring percorra as anotações que temos para executa-las
 public class SpringDataApplication implements CommandLineRunner{
 	
-	private CargoRepository repository;
+	private CrudCargoService cargoService;
 	private Boolean system = true;
 	
 	//Injeção de dependência
-	public SpringDataApplication(CargoRepository repository) {
-		this.repository = repository;
+	public SpringDataApplication(CrudCargoService cargoService) {
+		this.cargoService = cargoService;
 	}
 
 	public static void main(String[] args) {
@@ -32,12 +33,16 @@ public class SpringDataApplication implements CommandLineRunner{
 		while (system) {
 			System.out.println("Qual ação deseja executar? ");
 			System.out.println("0 - Sair.");
+			System.out.println("1 - Cargo.");
+			
+			int action = scanner.nextInt();
+			
+			if (action == 1) {
+				cargoService.inicial(scanner);
+			} else {
+				system = false;
+			}
 		}
-		
-		Cargo cargo = new Cargo();
-		cargo.setDescricao("DESENVOLVEDOR DE SOFTWARE");
-		
-		repository.save(cargo);
 	}
 
 }
--- src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
+++ src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
@@ -1,7 +1,10 @@
 package br.com.alura.spring.data.service;
 
+import java.util.Scanner;
+
 import org.springframework.stereotype.Service;
 
+import br.com.alura.spring.data.orm.Cargo;
 import br.com.alura.spring.data.repository.CargoRepository;
 
 @Service //indica para o Spring que é uma Service
@@ -11,5 +14,18 @@ public class CrudCargoService {
 	
 	public CrudCargoService(CargoRepository cargoRepository) {
 		this.cargoRepository = cargoRepository;
+	}
+	
+	public void inicial(Scanner scanner) {
+		salvar(scanner);
+	}
+	
+	private void salvar(Scanner scanner) {
+		System.out.println("Descrição do cargo");
+		String descricao = scanner.next();
+		Cargo cargo = new Cargo();
+		cargo.setDescricao(descricao);
+		cargoRepository.save(cargo);
+		System.out.println("Cargo salvo com sucesso!");
 	}
 }
