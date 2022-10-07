54b7d8dca8304d20835d86ccc5d704b04d245916
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-06 22:41:58 -0300

    CRUD pronto para cadastrar cargos

--- src/main/java/br/com/alura/spring/data/repository/CargoRepository.java
+++ src/main/java/br/com/alura/spring/data/repository/CargoRepository.java
@@ -1,7 +1,11 @@
 package br.com.alura.spring.data.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CargoRepository extends CrudRepository{
+import br.com.alura.spring.data.orm.Cargo;
+
+@Repository //para o Spring ler essa anotação
+public interface CargoRepository extends CrudRepository<Cargo, Integer>{
 
 }
