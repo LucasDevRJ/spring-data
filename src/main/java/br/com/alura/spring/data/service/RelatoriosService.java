e407b073d935f198ab5e84b5a798fdc7a9a61e2e
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-08 17:35:49 -0300

    Adição da classe de Relatórios junto com o método de pesquisar funcionário por nome

---
+++ src/main/java/br/com/alura/spring/data/service/RelatoriosService.java
@@ -0,0 +1,47 @@
+package br.com.alura.spring.data.service;
+
+import java.util.List;
+import java.util.Scanner;
+
+import org.springframework.stereotype.Service;
+
+import br.com.alura.spring.data.orm.Funcionario;
+import br.com.alura.spring.data.repository.FuncionarioRepository;
+
+@Service
+public class RelatoriosService {
+
+	private final FuncionarioRepository funcionarioRepository;
+	private Boolean system = true;
+	
+	public RelatoriosService(FuncionarioRepository funcionarioRepository) {
+		this.funcionarioRepository = funcionarioRepository;
+	}
+	
+	public void inicial(Scanner scanner) {
+		
+		while (system) {
+			System.out.println("Qual ação deseja executar? ");
+			System.out.println("1 - Buscar funcionário pelo nome.");
+			
+			int action = scanner.nextInt();
+			
+			switch (action) {
+			case 1:
+				buscarFuncionarioNome(scanner);
+			break;
+		
+			default:
+				system = false;
+				break;
+			}
+		}
+	}
+
+	private void buscarFuncionarioNome(Scanner scanner) {
+		System.out.println("Qual nome deseja pesquisar");
+		String nome = scanner.next();
+		List<Funcionario> funcionarios = funcionarioRepository.findByNome(nome);
+	}
+	
+}
