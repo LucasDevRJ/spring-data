7fc924e62d0387c6507b930cf26ad45c36efbd58
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-09 17:35:08 -0300

    Colocando o Scanner como parâmetro do método visualizar e pegando dados dele

--- src/main/java/br/com/alura/spring/data/service/CrudFuncionarioService.java
+++ src/main/java/br/com/alura/spring/data/service/CrudFuncionarioService.java
@@ -151,7 +151,10 @@ public class CrudFuncionarioService {
 		System.out.println("Alterado");
 	}
 	
-	private void visualizar() {
+	private void visualizar(Scanner scanner) {
+		System.out.println("Qual página você deseja visualizar");
+		Integer page = scanner.nextInt();
+		
 		Iterable<Funcionario> funcionarios = funcionarioRepository.findAll();
 		funcionarios.forEach(fn -> System.out.println(fn));
 	}
