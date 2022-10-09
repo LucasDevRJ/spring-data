8678c7e0b1bd6c7acab1c51a42bd14dc4a853a79
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-09 17:30:47 -0300

    Alteração do crud de funcionário para paginação

--- src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
+++ src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
@@ -5,17 +5,20 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import br.com.alura.spring.data.orm.Cargo;
 import br.com.alura.spring.data.orm.Funcionario;
 
-public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer>{
+public interface FuncionarioRepository extends PagingAndSortingRepository<Funcionario, Integer>{
 
 	List<Funcionario> findByNome(String nome);
 	
+	//Query criada com JPQL
 	@Query("SELECT f FROM Funcionario f WHERE f.nome = :nome AND f.salario >= :salario AND f.dataContratacao = :data") //indica que é uma anotação JPQL
 	List<Funcionario> findNomeSalarioMaiorDataContratacao(String nome, Double salario, LocalDate data);
 	
-	@Query(value = "SELECT f FROM Funcionario f WHERE f.dataContratacao > :data", nativeQuery = true)
+	//Query criada com a linguagem SQL normal
+	@Query(value = "SELECT * FROM funcionarios f WHERE f.data_contratacao >= :data", nativeQuery = true)
 	List<Funcionario> findFuncionarioDataContratacao(LocalDate data);
 }
